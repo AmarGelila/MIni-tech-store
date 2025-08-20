5d3fdc8c89e5dcb32e3ca2f7922d73cd49eaf358
amar <merogelila@gmail.com>
2025-08-20 07:17:52 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+**_ Main Functionalitties _**
+
+### Signup && Login
+
+### Search
+
+    - search query
+
+### Add to cart
+
+    - Quantity
+
+### Checkout && Payment
+
+
+
+**_ Main Tasks _**
+
+### Sample HTML Structure
+
+### CSS Style 'SASS'
+
+### Store Data in JSON file
+
+### Fetch Products
+
+### Display them in their sections
+
+### Add Event Listeners
---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+// const toggler = document.querySelector("#navbar-toggler");
+// const menu = document.querySelector(".menu");
+
+// toggler.addEventListener("click" , (e) => {
+//     console.log("here");
+//     e.stopPropagation();
+//     menu.classList.toggle("smallshow");
+// });
+
+// document.addEventListener("click" , (e) => {
+//     console.log(e.target);
+//     console.log(menu);
+//     if ( menu.contains(e.target) === false && e.target != menu );
+//         menu.classList.remove("smallshow");
+// });
